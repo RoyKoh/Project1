dfc4e90cae82d8bbf40e0e59697a0e0f1ab87d33
RoyKoh <i168168@yahoo.com.sg>
2015-11-19 17:13:21 +0800

    Changed data array in objects

--- proj1.js
+++ proj1.js
@@ -1,39 +1,140 @@
+var qnbank = [{
+  sno: 1,
+  question: 'What is the opposite of father?',
+  options: ['Sister', 'Auntie', 'Mother', 'Uncle'],
+  answer: 2
+},
+  {
+    sno: 2,
+    question: 'What is 1 + 2 ?',
+    options: ['2', '3', '6', '12'],
+    answer: 1
+  },
+  {
+    sno: 3,
+    question: 'What is 2 + 3 ?',
+    options: ['2', '3', '6', '5'],
+    answer: 3
+  },
+  {
+    sno: 4,
+    question: 'What is 3 + 4 ?',
+    options: ['7', '3', '6', '12'],
+    answer: 0
+  },
+  {
+    sno: 5,
+    question: 'What is 4 + 5 ?',
+    options: ['9', '3', '6', '12'],
+    answer: 0
+  },
+  {
+    sno: 6,
+    question: 'What is 5 + 6 ?',
+    options: ['2', '3', '6', '11'],
+    answer: 3
+  },
+  {
+    sno: 7,
+    question: 'What is 6 + 7 ?',
+    options: ['2', '3', '13', '12'],
+    answer: 2
+  },
+  {
+    sno: 8,
+    question: 'What is 7 + 8 ?',
+    options: ['2', '15', '6', '12'],
+    answer: 1
+  },
+  {
+    sno: 9,
+    question: 'What is 8 + 9 ?',
+    options: ['2', '17', '6', '12'],
+    answer: 1
+  },
+  {
+    sno: 10,
+    question: 'What is 9 + 10 ?',
+    options: ['2', '3', '19', '12'],
+    answer: 2
+  },
+  {
+    sno: 11,
+    question: 'What is 10 + 11 ?',
+    options: ['2', '3', '21', '12'],
+    answer: 2
+  },
+  {
+    sno: 12,
+    question: 'What is 11 + 12 ?',
+    options: ['2', '23', '6', '12'],
+    answer: 1
+  },
+  {
+    sno: 13,
+    question: 'What is 12 + 13 ?',
+    options: ['25', '3', '6', '12'],
+    answer: 0
+  },
+  {
+    sno: 14,
+    question: 'What is 13 + 14 ?',
+    options: ['27', '3', '6', '12'],
+    answer: 0
+  },
+  {
+    sno: 15,
+    question: 'What is 14 + 15 ?',
+    options: ['2', '3', '29', '12'],
+    answer: 2
+  },
+  {
+    sno: 16,
+    question: 'What is 15 + 16 ?',
+    options: ['2', '31', '6', '12'],
+    answer: 1
+  },
+  {
+    sno: 17,
+    question: 'What is 16 + 17 ?',
+    options: ['33', '3', '6', '12'],
+    answer: 0
+  },
+  {
+    sno: 18,
+    question: 'What is 17 + 18 ?',
+    options: ['2', '3', '6', '35'],
+    answer: 3
+  },
+  {
+    sno: 19,
+    question: 'What is 18 + 19 ?',
+    options: ['2', '3', '6', '37'],
+    answer: 3
+  },
+  {
+    sno: 20,
+    question: 'What is 19 + 20 ?',
+    options: ['2', '39', '6', '12'],
+    answer: 1
+  }
+  ]
 
-function MyReason(){
-    var reason = prompt("Reason");
-    document.getElementById("reason").value = reason;
-    document.getElementById("form").submit();
-}
+// var paragraph = document.createElement('p')
+// paragraph.textContent = '... blablabla ...'
+// document.body.appendChild(paragraph)
 
-function Retry(){
-prompt("Do you want to retry? Y(es) to refresh and N(o) to send result to your parent by selected media (Y/N)")
-}
-<button onclick="getElementById('demo').innerHTML=Date()">What is the time?</button>
+// var pDiv = document.getElementById("paragraphs");
 
-
-// Button click before show date time on html page //
-<p>Click the button to display the time.</p>
-<button onclick="getElementById('demo').innerHTML=Date()">What is the time?</button>
-<p id="demo"></p>
-
-
-// To get input from text box and assign to a variable //
-<script type = "text/javascript">
-function doIt(val) {
-document.getElementById("theResult").innerHTML = val;
-}
-</script>
-
-<input type = "text" id = "txt" onblur = "doIt(this.value)">
-<br><br>
-<span id = "theResult"></span>
-
-onkeyup="res(this,'abcd');"
-
-<p id="demo" onclick="myFunction()">Click me to change my HTML content (innerHTML).</p>
-
-<script>
-function myFunction() {
-    document.getElementById("demo").innerHTML = "Paragraph changed!";
-}
-</script>
+// Create a new div if not exist
+// if (!pDiv) {
+//              pDiv = document.createElement("div");
+//              pDiv.id = "paragraphs";
+//              pDiv.innerHTML = "<p>Paragraph 1</p>";
+//              document.body.appendChild(pDiv);
+//            }
+//
+//            var p = document.createElement("p");
+//            var t = document.createTextNode(<p>Paragraph 2</p>);
+//            p.appendChild(t);
+//            pDiv.appendChild(p);
