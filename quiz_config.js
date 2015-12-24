c0645e031e760212da0dcd8652aa04fb89044333
RoyKoh <i168168@yahoo.com.sg>
2015-12-24 15:11:48 +0800

    Remove unwanted files and modi files

--- quiz_config.js
+++ quiz_config.js
@@ -1,10 +1,10 @@
 // defining null array variables for questions, choices, answers, response.
 // Use [] instead of new Array()
 
-var questions = [];
-var choices = [];
-var answers = [];
-var response = [];
+var questions = []
+var choices = []
+var answers = []
+var response = []
 
 // To add more questions, just follow the format below.
 // Question 1 value string is assigned to question[0] in javascript vector convention
@@ -14,80 +14,79 @@ var response = [];
 
 // Below DB created in MongoLab as documents
 
-questions[0] = "1) JavaScript is ...";
-choices[0] = [];
-choices[0][0] = "the same as Java";
-choices[0][1] = "kind of like Java";
-choices[0][2] = "different than Java";
-choices[0][3] = "ther written part of Java";
-answers[0] = choices[0][2];
+questions[0] = '1) JavaScript is ...'
+choices[0] = []
+choices[0][0] = 'the same as Java'
+choices[0][1] = 'kind of like Java'
+choices[0][2] = 'different than Java'
+choices[0][3] = 'ther written part of Java'
+answers[0] = choices[0][2]
 
-questions[1] = "2) JavaScript is ...";
-choices[1] = [];
-choices[1][0] = "subjective";
-choices[1][1] = "objective";
-choices[1][2] = "evil";
-choices[1][3] = "object based";
-answers[1] = choices[1][3];
+questions[1] = '2) JavaScript is ...'
+choices[1] = []
+choices[1][0] = 'subjective'
+choices[1][1] = 'objective'
+choices[1][2] = 'evil'
+choices[1][3] = 'object based'
+answers[1] = choices[1][3]
 
-questions[2] = "3) To comment out a line in JavaScript ...";
-choices[2] = [];
-choices[2][0] = "Precede it with two forward slashes, i.e. '//'";
-choices[2][1] = "Precede it with an asterisk and a forward slash, i.e. '*/'";
-choices[2][2] = "Precede it with an asterisk, i.e. '*'";
-choices[2][3] = "Precede it with a forward slash and an asterisk, i.e. '/*'";
-answers[2] = choices[2][0];
+questions[2] = '3) To comment out a line in JavaScript ...'
+choices[2] = []
+choices[2][0] = 'Precede it with two forward slashes, i.e. //'
+choices[2][1] = 'Precede it with an asterisk and a forward slash, i.e. */'
+choices[2][2] = 'Precede it with an asterisk, i.e. *'
+choices[2][3] = "Precede it with a forward slash and an asterisk, i.e. '/*'"
+answers[2] = choices[2][0]
 
-questions[3] = "4) JavaScript can only run on Windows";
-choices[3] = [];
-choices[3][0] = "True";
-choices[3][1] = "False";
-answers[3] = choices[3][1];
+questions[3] = '4) JavaScript can only run on Windows'
+choices[3] = []
+choices[3][0] = 'True'
+choices[3][1] = 'False'
+answers[3] = choices[3][1]
 
-questions[4] = "5) Semicolons are optional at the end of a JavaScript statement.";
-choices[4] = [];
-choices[4][0] = "True";
-choices[4][1] = "False";
-answers[4] = choices[4][0];
+questions[4] = '5) Semicolons are optional at the end of a JavaScript statement.'
+choices[4] = []
+choices[4][0] = 'True'
+choices[4][1] = 'False'
+answers[4] = choices[4][0]
 
-questions[5] = "6) The four basic data types are:";
-choices[5] = [];
-choices[5][0] = "strings, numbers, BooBoos, and nulls";
-choices[5][1] = "strings, text, Booleans, and nulls";
-choices[5][2] = "strings, numbers, Booleans, and nulls";
-choices[5][3] = "strings, numbers, Booleans, and zeros";
-answers[5] = choices[5][2];
+questions[5] = '6) The four basic data types are:'
+choices[5] = []
+choices[5][0] = 'strings, numbers, BooBoos, and nulls'
+choices[5][1] = 'strings, text, Booleans, and nulls'
+choices[5][2] = 'strings, numbers, Booleans, and nulls'
+choices[5][3] = 'strings, numbers, Booleans, and zeros'
+answers[5] = choices[5][2]
 
-questions[6] = "7) What is the opposite of father?";
-choices[6] = [];
-choices[6][0] = "molder";
-choices[6][1] = "mother";
-choices[6][2] = "fatherer";
-choices[6][3] = "more";
-answers[6] = choices[6][1];
+questions[6] = '7) What is the opposite of father?'
+choices[6] = []
+choices[6][0] = 'molder'
+choices[6][1] = 'mother'
+choices[6][2] = 'fatherer'
+choices[6][3] = 'more'
+answers[6] = choices[6][1]
 
-questions[7] = "8) What is the opposite of mother?";
-choices[7] = [];
-choices[7][0] = "father";
-choices[7][1] = "rather";
-choices[7][2] = "blabble";
-choices[7][3] = "gargle";
-answers[7] = choices[7][0];
-
+questions[7] = '8) What is the opposite of mother?'
+choices[7] = []
+choices[7][0] = 'father'
+choices[7][1] = 'rather'
+choices[7][2] = 'blabble'
+choices[7][3] = 'gargle'
+answers[7] = choices[7][0]
 
 // response for getting 100%
-response[0] = "Excellent, top marks!";
+response[0] = 'Excellent, top marks!'
 // response for getting 90% or more
-response[1] = "Excellent, try again to get 100%!"
+response[1] = 'Excellent, try again to get 100%!'
 // response for getting 70% or more
-response[2] = "Well done, that is a good score, can you do better?";
+response[2] = 'Well done, that is a good score, can you do better?'
 // response for getting over 50%
-response[3] = "Nice one, you got more than half of the questions right, can you do better?";
+response[3] = 'Nice one, you got more than half of the questions right, can you do better?'
 // response for getting 40% or more
-response[4] = "You got some questions right, you can do better!";
+response[4] = 'You got some questions right, you can do better!'
 // response for getting 20% or more
-response[5] = "You didn't do too well, why not try again!?";
+response[5] = 'You did not do too well, why not try again?'
 // response for getting 10% or more
-response[6] = "That was pretty poor!  Try again to improve!";
+response[6] = 'That was pretty poor!  Try again to improve!'
 // response for getting 9% or less
-response[7] = "Oh dear, I think you need to go back to school (or try again)!";
+response[7] = 'Oh dear, I think you need to go back to school (or try again)!'
--- server.js
+++ server.js
@@ -13,7 +13,7 @@
 // var server = http.createServer(handleRequest);
 // start our server
 // server.listen(PORT, function(){
-    //Callback triggered when server is successfully listening.
+// Callback triggered when server is successfully listening.
 //    console.log("Server listening on: http://localhost:%s", PORT);
 // });
 // fs.readFile('index.html', function (err, html) {
@@ -26,14 +26,12 @@
 //       response.end();
 //   }).listen(1337, '127.0.0.1');
 // });
-
-
 // Using Express npm module to start a static server listening at port 5000 or process env at remote Server
 // Procfile ==>  web: node app.js
 // package.json  ==>  on "main": "app.js"
 // .gitignore ==> node_modules and also *.log
-var express = require('express');
-var app = express();
-app.use(express.static(__dirname + '/'));
-var port = process.env.PORT || 5000;
-app.listen(port);
+var express = require('express')
+var app = express()
+app.use(express.static(__dirname + '/'))
+var port = process.env.PORT || 5000
+app.listen(port)
