dcc3be66ffa2e35a3e069cd4b80e03128e84e726
RoyKoh <i168168@yahoo.com.sg>
2015-12-27 20:08:41 +0800

    Include AppendChild

--- quiz_functions.js
+++ quiz_functions.js
@@ -1,122 +1,129 @@
-// set user answers to a new null array variable, number of questions answered variable to 0
-var useranswers = []
-var answered = 0
-var questions = []
-var i
-var j
-var pc
-var choices
-var answers
-var response
-var alertMsg
-var correct
-var incorrect
-var confirm
-var alert
-var questionCount
-
-// function renderQuiz() called in index.html rendered as a display of mcq with button response
-function renderQuiz () {
-  // use for loop iterate through questions() array data bank and render display of radio buttons and response pending click responses
-  for (i = 0; i < questions.length; i++) {
-    // display on console starting i=0, for questions, display questions[0] and set initial blank.gif for initial null result status
-    document.writeln('<p class="question">' + questions[i] + ' <span id="result_' + i + '"><img src="image/blank.gif" style="border:0" alt="" /></span></p>')
-    // use for loop iterate through display radio buttons and number of choices, iteration restricted by choices[i], the number of iterations same as questions[]
-    // display radio buttons set answer varaiables onclick
-    for (j = 0; j < choices[i].length; j++) {
-      document.writeln('<input type="radio" name="answer_' + i + '" value="' + choices[i][j] + '" id="answer_' + i + '_' + j + '" class="question_' + i + '" onclick="submitAnswer(' + i + ', this, \'question_' + i + '\', \'label_' + i + '_' + j + '\')" /><label id="label_' + i + '_' + j + '" for="answer_' + i + '_' + j + '"> ' + choices[i][j] + '</label><br />')
+(function (quizArray, qpp, reslink) {
+  var ent
+  var perPage = 0
+  var n1 = 109
+  var n2 = 13
+  var reslink = ''
+  var qsParm = []
+  function qs () {
+    var query = window.location.search.substring(1)
+    var parms = query.split('&')
+    for (var i = 0; i < parms.length; i++) {
+      var pos = parms[i].indexOf('=')
+      if (pos > 0) {
+        var key = parms[i].substring(0, pos)
+        var val = parms[i].substring(pos + 1)
+        qsParm[key] = val
+      }
     }
   }
-  // display button show score, on click run function showScore().  Display button Reset Quiz, onclick run function resetQuiz(true)
-  document.writeln('<p><input type="submit" value="Show Score" onclick="showScore()" /> <input type="submit" value="Reset Quiz" onclick="resetQuiz(true)" /></p><p style="display:none"><img src="image/correct.gif" style="border:0" alt="Correct!" /><img src="image/incorrect.gif" style="border:0" alt="Incorrect!" /></p>')
-}
-// end of function renderQuiz()
-
-// function resetQuiz() on prompt reset quiz to blank and start accepting responses
-function resetQuiz (showConfirm) {
-  if (showConfirm) {
-    if (!confirm('Are you sure you want to reset your answers and start from the beginning?')) {
+  qsParm['qnum'] = 0
+  qsParm['cor'] = 0
+  qsParm['fin'] = 0
+  qs()
+  var qnum = qsParm['qnum']
+  var cor = qsParm['cor']
+  cor = cor % n1
+  function loadQuiz () {
+    if (qnum >= quizArray.length) displayResult()
+    else {
+      displayQuiz(ent = document.getElementById('quiz1'), qnum++)
+      for (var j = 2; j <= qpp; j++) {
+        if ((ent = document.getElementById('quiz' + j)) && qnum < quizArray.length) displayQuiz(ent, qnum++)
+      }
+      var newB = document.createElement('button')
+      newB.style.styleFloat = 'right'
+      newB.style.cssFloat = 'right'
+      newB.onclick = function () {
+        nextPage(qnum)
+        return false
+      }
+      var newTb = document.createTextNode('Next >>')
+      newB.appendChild(newTb)
+      ent.appendChild(newB)
+    }
+  }
+  function displayQuiz (ent, qnum) {
+    perPage++
+    var qna = quizArray[qnum].split('~')
+    var newF = document.createElement('form')
+    var newDq = document.createElement('div')
+    newDq.className = 'question'
+    newDq.appendChild(document.createTextNode(Number(qnum + 1) + ': ' + qna[2]))
+    newF.appendChild(newDq)
+    newDq = document.createElement('div')
+    newDq.className = 'answers'
+    for (var i = 3; qna[i] != null && qna[i] !== ''; i++) {
+      var newDa = document.createElement('label')
+      newDa.htmlFor = 'a' + qnum + i
+      /* @cc_on @if (@_jscript) var newR = document.createElement("<input name='a"+qnum+"'>"); @else */
+      var newR = document.createElement('input')
+      newR.name = 'a' + qnum
+      /* @end @*/
+      newR.type = 'radio'
+      newR.id = 'a' + qnum + i
+      newR.value = i
+      newDa.appendChild(newR)
+      newDa.appendChild(document.createTextNode(' ' + qna[i] + ' '))
+      newDq.appendChild(newDa)
+    }
+    newF.appendChild(newDq)
+    document.getElementById('quiz' + perPage).appendChild(newF)
+  }
+  function displayResult () {
+    var newP = document.createElement('p')
+    newP.style.textAlign = 'center'
+    var newB = document.createElement('b')
+    newB.appendChild(document.createTextNode('Congratulations.'))
+    newB.appendChild(document.createElement('br'))
+    newB.appendChild(document.createTextNode(' You have completed the quiz.'))
+    newP.appendChild(newB)
+    newP.appendChild(document.createElement('br'))
+    newP.appendChild(document.createElement('br'))
+    newP.appendChild(document.createTextNode('You answered ' + cor + ' of the ' + qnum + ' questions correctly.'))
+    if (reslink !== '') {
+      newP.appendChild(document.createElement('br'))
+      var newA = document.createElement('a')
+      newA.href = reslink + '?cor=' + cor + '&qnum=' + qnum
+      newA.appendChild(document.createTextNode('Find Out More'))
+      newP.appendChild(newA)
+    }
+    document.getElementById('quiz1').appendChild(newP)
+    var newC = document.createElement('button')
+    newC.style.styleFloat = 'right'
+    newC.onclick = function () {
+      cor = 0
+      nextPage(0)
       return false
     }
-    document.location = document.location
+    newC.appendChild(document.createTextNode('Try Again'))
+    document.getElementById('quiz1').appendChild(newC)
   }
-}
-// end of resetQuiz()
-
-// function submitAnswer() assigns object value to useranswers[], set bold, call functions disableQuestion(),showResult() and accumulate answered variable
-function submitAnswer (questionId, obj, classId, labelId) {
-  useranswers[questionId] = obj.value
-  document.getElementById(labelId).style.fontWeight = 'bold'
-  disableQuestion(classId)
-  showResult(questionId)
-  answered++
-}
-// end of function submitAnswer()
-
-// function showResult() display relevant gif on click
-function showResult (questionId) {
-  if (answers[questionId] === useranswers[questionId]) {
-    document.getElementById('result_' + questionId).innerHTML = '<img src="image/correct.gif" style="border:0" alt="Correct!" />'
-  } else {
-    document.getElementById('result_' + questionId).innerHTML = '<img src="image/incorrect.gif" style="border:0" alt="Incorrect!" />'
-  }
-}
-// end of function showresult()
-
-// function showScore() display score on click and alert results
-function showScore () {
-  if (answered !== answers.length) {
-    alert('You have not answered all of the questions yet!')
-    return false
-  }
-  questionCount = answers.length
-  correct = 0
-  incorrect = 0
-  for (i = 0; i < questionCount; i++) {
-    if (useranswers[i] === answers[i]) {
-      correct++
-    } else {
-      incorrect++
+  function checkAnswer (e, b, g, q) {
+    var a = -1
+    var x = (b%n2) + 2
+    for (var i = 3; document.getElementById(e + i); i++) {
+      if (document.getElementById(e + i).checked) {
+        a = i - 3
+      }
     }
+    var c = (g % x)
+    return a === c ? 1 : 0
   }
-  pc = Math.round((correct / questionCount) * 100)
-  alertMsg = 'You scored ' + correct + ' out of ' + questionCount + '\n\n'
-  alertMsg += 'You correctly answered ' + pc + '% of the questions! \n\n'
-  if (pc === 100) {
-    alertMsg += response[0]
-  } else if (pc >= 90) {
-    alertMsg += response[1]
-  } else if (pc >= 70) {
-    alertMsg += response[2]
-  } else if (pc > 50) {
-    alertMsg += response[3]
-  } else if (pc >= 40) {
-    alertMsg += response[4]
-  } else if (pc >= 20) {
-    alertMsg += response[5]
-  } else if (pc >= 10) {
-    alertMsg += response[6]
-  } else {
-    alertMsg += response[7]
-  }
-  if (pc < 100) {
-    if (confirm(alertMsg)) {
-      resetQuiz(false)
-    } else {}
-    return false
-  } else {
-    alert(alertMsg)
-  }
-}
-// end of function showScore()
-
-// function disableQuestion() disallow further click after showing gif result
-function disableQuestion (classId) {
-  var alltags = document.all ? document.all : document.getElementsByTagName('*')
-  for (i = 0; i < alltags.length; i++) {
-    if (alltags[i].className === classId) {
-      alltags[i].disabled = true
+  function nextPage (qnum) {
+    for (var i = qnum-perPage; i < qnum; i++) {
+      var qna = quizArray[i].split('~')
+      cor += checkAnswer('a' + i, qna[1], qna[0], i)
     }
+    var thispage = self.location.href
+    var www = thispage.lastIndexOf('?')
+    if (www !== -1) thispage = thispage.substr(0, www)
+    var p = Math.floor((Math.random() * 8) + 2)
+    var m = (p * n1) + cor
+    var nxt = thispage + '?qnum=' + qnum + '&cor=' + m
+    if (qnum >= quizArray.length) nxt += '&fin=1'; top.location.href = nxt
   }
+  loadQuiz()
 }
-// end of disableQuestion()
+)
+(quizArray, qpp, reslink)
